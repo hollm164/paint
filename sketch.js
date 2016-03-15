048e7ed59e9814775d216265b35de52a237290b5
hollm164 <marielleholland@marielles-mbp.wireless.newschool.edu>
2016-03-15 17:29:17 -0400

    done!

--- sketch.js
+++ sketch.js
@@ -6,11 +6,24 @@
 var drawingCanvas;
 var saveButton;
 var brushPicker;
+var clearButton;
+
+
 
 //values saved from html elements
 var colorPicker;
 var brushSize;
 var brushType;
+
+var pumpkin;
+
+var uploadedImage;
+
+function preload(){
+  pumpkin = loadImage("images/pumpkin.png");
+}
+
+
 
 
 function setup() {
@@ -32,7 +45,8 @@ function setup() {
     saveButton.mouseClicked(saveFunction);
 
     //TASK: set up the clear button
-
+    clearButton = select('.clearButton');
+    clearButton.mouseClicked(clearFunction);
 
     //set up the brush types
     brushPicker = createSelect();
@@ -40,14 +54,21 @@ function setup() {
 
     brushPicker.option('paint brush');
     //TASK: add paint bucket option
+    brushPicker.option('paint bucket');
     //TASK: add eraser option
+    brushPicker.option('eraser');
     //TASK: add two new brush options
+    brushPicker.option('pumpkin');
+
+    brushPicker.option('spray paint');
 
     //Set up the brush type event listener:
     brushPicker.changed(changeBrush);
 
     //Set the default brush type to the first item in the menu:
     brushType = brushPicker.value();
+
+    var uploadButton = createFileInput(imageUploaded);
 }
 
 
@@ -56,6 +77,14 @@ function draw() {
     if (mouseIsPressed) {
         if (brushType == "paint brush"){
             standardStroke();
+        } else if (brushType == "paint bucket"){
+            fillBackground();
+        } else if (brushType == "eraser"){
+            eraseDrawing();
+        } else if (brushType == "pumpkin"){
+            drawPumpkin();
+        } else if (brushType == "spray paint"){
+            sprayCan();
         }
         //add your other brush options here using else if
 
@@ -64,7 +93,18 @@ function draw() {
         //if you use an image, the recommended size is 16x16 or 32x32 pixels
         cursor(CROSS);
     }
+
+  }
+
+//callback function
+function imageUploaded(file){
+	uploadedImage = loadImage(file.data, drawImg);
 }
+
+function drawImg(){
+	image(uploadedImage,0,0);
+}
+
 
 //--------------------------
 // Brushes
@@ -95,6 +135,30 @@ function standardStroke(){
 // Event Listeners
 //--------------------------
 
+function fillBackground(){
+  background("#"+colorPicker.value());
+}
+
+function eraseDrawing(){
+  strokeWeight(brushSize.value());
+  stroke("white");
+  line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+function drawPumpkin(){
+  imageMode(CENTER);
+  image(pumpkin,mouseX,mouseY, pumpkin.width, pumpkin.height);
+}
+
+function sprayCan(){
+  for(var i = 0; i< 30; i++){
+    noStroke();
+    fill("#"+colorPicker.value());
+    ellipse (random(mouseX-brushSize.value(),mouseX+brushSize.value()), random(mouseY-brushSize.value(),mouseY+brushSize.value()),2,2);
+  }
+
+}
+
 function changeBrush(){
     //This takes the name of the drop-down item you selected
     //and saves it as a string to the brushType variable.
@@ -107,3 +171,7 @@ function saveFunction() {
 }
 
 //TASK: set up clear button function
+function clearFunction(){
+    clear (drawingCanvas);
+    background("white");
+}
